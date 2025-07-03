1694744aba142a13d568b47d632aff2c0126d72a
Pedro Schinke <pedro.schinke@outlook.com>
2025-07-02 21:44:23 -0300

    criadas novas rotas de autenticacao

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::resource('user', UserController::class)->only(['index', 'store']);
+Route::get('user/infos/{user_id}', [UserController::class, 'getInfos']);
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('user', [AuthController::class, 'me']);
+    Route::post('logout', [AuthController::class, 'logout']);
+});
