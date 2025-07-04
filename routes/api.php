45443fb3c9554ec9997307a67690864d06713e9e
Pedro Schinke <pedro.schinke@outlook.com>
2025-07-03 22:34:22 -0300

    nova rota para favoritar

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,27 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FavoritoController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('user/infos/{user_id}', [UserController::class, 'getInfos']);
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:api')->group(function () {
+    /**
+     * Rotas de dados do usuário
+     */
+    Route::get('user/infos/{user_id}', [UserController::class, 'getInfos']);
+
+    /**
+     * Rotas de autenticação do usuário
+     */
     Route::get('user', [AuthController::class, 'me']);
     Route::post('logout', [AuthController::class, 'logout']);
+
+    /**
+     * Rotas de favoritos
+     */
+    Route::post('favoritos', [FavoritoController::class, 'toggleFavorito']);
 });
